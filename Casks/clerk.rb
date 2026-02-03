7fb034f3a3e30b13b68365886c9508635e0926d9
Theo Zourzouvillys <zourzouvillys@users.noreply.github.com>
2026-02-02 16:40:15 -0800

    Fix Cask: move pkg stanza inside arch blocks

--- Casks/clerk.rb
+++ Casks/clerk.rb
@@ -4,19 +4,18 @@ cask "clerk" do
   on_intel do
     sha256 "26eaa5fe6011c8ec9be4d19ea1c06c8c43ddde2ef49763a283b508877a22fd46"
     url "https://github.com/clerk/cli/releases/download/v#{version}/clerk-v#{version}-darwin-amd64.pkg"
+    pkg "clerk-v#{version}-darwin-amd64.pkg"
   end
 
   on_arm do
     sha256 "877c6b4ab158ff6e8c764a4ff067a213c19bca54ad644203db7b2285c362d356"
     url "https://github.com/clerk/cli/releases/download/v#{version}/clerk-v#{version}-darwin-arm64.pkg"
+    pkg "clerk-v#{version}-darwin-arm64.pkg"
   end
 
   name "Clerk CLI"
   desc "Command-line interface for Clerk"
   homepage "https://clerk.com"
-
-  # The pkg installs to /usr/local/bin/clerk
-  pkg "clerk-v#{version}-darwin-#{arch == :arm64 ? 'arm64' : 'amd64'}.pkg"
 
   uninstall pkgutil: "com.clerk.cli"
 
